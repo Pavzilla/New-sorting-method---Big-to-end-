83e63ce699891d7c5c26d2091eb2297bbc9e632a
Pavzilla <130251336+Pavzilla@users.noreply.github.com>
2023-04-09 00:08:00 +0300

    Add files via upload

---
+++ New_sorting_method_Big_to_end.py
@@ -0,0 +1,14 @@
+﻿list1 = [1, 5, 4, 8, 6, 20, 100, 4, 50, 255, 41, 758, 0 ]
+n=len(list1)
+
+while True:
+    per = False
+    for i in range(n-1):
+        if list1[i] > list1[i+1]:
+             list1.append(list1[i])
+             list1.pop(i)               
+             per = True
+    if not per:
+        break
+print(list1)
+
